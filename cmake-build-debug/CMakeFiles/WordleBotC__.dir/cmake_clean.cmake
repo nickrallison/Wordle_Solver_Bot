23757e7322501c56950baf9865b2e7f51c3f386e
nickrallison <99363282+nickrallison@users.noreply.github.com>
2022-02-26 13:53:22 -0700

    Changing CMakeDebug

---
+++ cmake-build-debug/CMakeFiles/WordleBotC__.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WordleBotC__.dir/main.cpp.o"
+  "WordleBotC__.exe"
+  "WordleBotC__.pdb"
+  "libWordleBotC__.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WordleBotC__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
